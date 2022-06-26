9fe6be029afec112f918ff42ccbb6052acdfe7fe
rohitsingh385 <rk301855@gmail.com>
2022-06-26 23:29:47 +0530

    day 8 2 questions

---
+++ day8.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include<vector>
+#include <climits>
+using namespace std;
+
+// ############## MAX MIN FIND?
+
+/*int main(){
+
+	int n;
+	cin>>n;
+
+	int arr[n];
+
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	int max= INT_MIN;
+	int min = INT_MAX;
+
+	for(int i=0;i<n;i++){
+		if(arr[i]>max){
+			max = arr[i];
+		}if(arr[i]<min){
+			min = arr[i];
+		}
+	}
+	cout<<max<<" "<<min<<" ";
+
+	return 0;
+}*/
+
+
+// ########## RUNNING SUM OF ARRAY
+
+/*int main(){
+	int n;
+	cin>>n;
+
+	int arr[n];
+	
+
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+
+	for(int i=0;i<n;i++){
+		arr[i] = arr[i] + arr[i-1];
+	}
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ";
+	}
+
+}*/
+
