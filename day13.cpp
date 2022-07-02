3daae4ecc6c2d74c3b24f6995e9490e130316692
rohitsingh385 <rk301855@gmail.com>
2022-07-02 15:12:11 +0530

    day 13 updated

--- day13.cpp
+++ day13.cpp
@@ -3,37 +3,188 @@
 
 using namespace std;
 
+// ############################ MAX SUBARRAY
+/*
 int main(){
 	int n;
 	cin>>n;
 
-	int a[n];
+	int arr[n];
 
 	for(int i=0;i<n;i++){
-		cin>>a[i];
+		cin>>arr[i];
 	}
-
-	const int N = 1e6+2;
-
-	int idx[N];
-
-	for(int i=0;i<N;i++){
-		idx[i] = -1;
-	}
-
-	int minidx = INT_MAX;
 
 	for(int i=0;i<n;i++){
-		if(idx[a[i]] != -1){
-			minidx = min(minidx, idx[a[i]]);
-		}else{
-			idx[a[i]] = i;
+		for(int j=i;j<n;j++){
+			for(int k=i;k<=j;k++){
+				cout<<arr[k]<<" ";
+			}cout<<endl;
 		}
 	}
 
-	if(minidx == INT_MAX){
-		cout<<"-1"<<endl;
-	}else{
-		cout<<minidx + 1<<endl;
+
+}*/
+
+// ################# MAXIMUM SUBARRAY BRUTEFORCE
+
+/*int main(){
+	int n;
+	cin>>n;
+
+	int arr[n];
+
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
 	}
+
+	int maxSum = INT_MIN;
+
+	for(int i=0;i<n;i++){
+		for(int j=i;j<n;j++){
+			int sum=0;
+			for(int k=i;k<=j;k++){
+				sum+=arr[k];
+			}
+			maxSum = max(maxSum,sum);
+		}
+	}
+		cout<<maxSum<<" ";
 }
+*/
+
+// ########################### MAXIMUM SUBARRAY optimal apporach using KADANE'S ALGO
+
+
+/*int main(){
+	int n;
+	cin>>n;
+
+	int arr[n];
+
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+
+	int crrSum =0;
+	int maxSUm = INT_MIN;
+
+	for(int i=0;i<n;i++){
+		crrSum+=arr[i];
+		if(crrSum<0){
+			crrSum =0;
+		}
+
+		maxSUm = max(maxSUm,crrSum);
+
+	}
+	cout<<maxSUm<<" ";
+
+
+}
+
+*/
+
+// ########################### MAXIMUM CIRCULAR SUBARRAY SUM
+
+/*int kadaane(int arr[],int n){
+
+	int crrSum = 0;
+	int mxSum = INT_MIN;
+
+	for(int i=0;i<n;i++){
+		crrSum+=arr[i];
+		if(crrSum<0){
+			crrSum = 0;
+		}
+        mxSum = max(mxSum,crrSum);
+
+	}
+	return mxSum;
+}
+
+
+int main(){
+	int n;
+	cin>>n;
+
+	int arr[n];
+
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+
+	int wrapsum;
+	int nonWrapsum;
+
+	nonWrapsum = kadaane(arr,n);
+    
+    int totalSum=0;
+
+	for(int i=0;i<n;i++){
+		totalSum+=arr[i];
+		arr[i] = -arr[i];
+	}
+	wrapsum = totalSum + kadaane(arr,n);
+
+	cout<<max(wrapsum,nonWrapsum)<<' ';
+} 
+*/
+
+// ########################### PAIR SUM PROBLEM BRUTEFORCE
+
+/*bool pairSu(int n,int arr[],int key){
+
+	for(int i=0;i<n;i++){
+		for(int j=i+1;j<n;j++){
+			if(arr[i]+arr[j]==key){
+				cout<<i<<" "<<j<<endl;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+int main(){
+	int arr[] = {2 ,4 ,7 ,11 ,14 ,16 ,20 ,21};
+	int key = 31;
+
+	cout<<pairSu(8,arr,key);
+
+
+}*/
+
+
+// ################################# PAIR SUM O(N)
+// when array is sorted
+
+
+bool pairSu(int n, int arr[],int key){
+
+	int low = 0;
+	int high = n-1;
+
+	while(low<high){
+
+		if(arr[low]+arr[high]==key){
+			cout<<low<<" "<<high<<endl;
+			return true;
+		}
+		else if(arr[low]+arr[high]>key){
+			high--;
+		}else{
+			low++;
+		}
+	}
+	return false;
+}
+
+int main(){
+	int arr[] = {2 ,4 ,7 ,11 ,14 ,16 ,20 ,21};
+	int key = 31;
+
+	cout<<pairSu(8,arr,key);
+
+}
+
