b17cf85e7c5f79de326b5421a3b60d2cfd9ab54a
rohitsingh385 <rk301855@gmail.com>
2022-07-12 22:23:07 +0530

    day 24 today i did 3 advanced questions on recursion

---
+++ day24.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+// #################### PRINT ALL PREMUTATIONS OF A STRING
+/*
+void permutation(string s,string ans){
+	if(s.length()==0){
+		cout<<ans<<endl;
+		return;
+	}
+
+	for(int i=0;i<s.length();i++){
+		char ch = s[i];
+
+		string ros = s.substr(0,i)+s.substr(i+1);
+		permutation(ros,ans+ch);
+
+	}
+}
+
+int main(){
+
+	permutation("ABC","");
+
+
+
+}*/
+
+
+// ################   board game problem
+/*
+int countpath(int s,int e){
+	if(s==e){
+		return 1;
+	}
+	if(s>e){
+		return 0;
+	}
+    int count =0;
+	for(int i=1;i<6;i++){
+		count+=countpath(s+i,e);
+	}
+	return count;
+}
+
+int main(){
+	cout<<countpath(0,3)<<endl;
+}*/
+
+
+int countpathmaze(int n,int i,int j){
+	if(i==n-1 && j==n-1){
+		return 1;
+	}
+
+	if(i>=n || j>=n){
+		return 0;
+	}
+
+	return (countpathmaze(n,i+1,j)+countpathmaze(n,i,j+1));
+}
+
+int main(){
+	cout<<countpathmaze(3,0,0)<<endl;
+}
